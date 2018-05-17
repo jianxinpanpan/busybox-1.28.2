4cb0bd87690b7477204339fcb3c77d190f84097a
jianxinpanpan <jianxin_pan@163.com>
2018-05-16 22:29:34 -0700

    mod config and script
    Signed-off-by: jianxinpanpan <jianxin_pan@163.com>

--- mk.sh
+++ mk.sh
@@ -1,5 +1,3 @@
-
-
 make defconfig
 make CROSS_COMPILE=arm-linux-gnueabi-
-make install CROSS_COMPILE=arm-linux-gnueabi-
+make install CROSS_COMPILE=arm-linux-gnueabi- CONFIG_PREFIX=`pwd`/../tiny_tools/busybox-1.28_bin/
